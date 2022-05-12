97b1f3570edbcfb3c6a1c7ebbee2dcf5e896c1eb
ThomasSourbeI <thomas.sourbe@eleve.isep.fr>
2022-05-12 11:08:05 +0200

    ajout de murs

---
+++ src/main/java/com/ricochetrobots/entities/Cell.java
@@ -0,0 +1,8 @@
+package com.ricochetrobots.entities;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Cell {
+
+}
--- src/main/java/com/ricochetrobots/entities/Grid.java
+++ src/main/java/com/ricochetrobots/entities/Grid.java
@@ -1,4 +1,33 @@
 package com.ricochetrobots.entities;
 
+import com.ricochetrobots.systems.GameController;
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 public class Grid {
+    //private int[][] board_matrix;
+    //private int board_size;
+    //private int win_length;
+
+    //public Grid(int board_size, int win_length) {
+        //this.board_matrix = new int[board_size][board_size];
+        //this.board_size = board_size;
+        //this.win_length = win_length;
+
+        //for (int i = 0; i < board_size; i++) {
+            //for (int j = 0; j < board_size; j++) {
+                //this.board_matrix[i][j] = 0;
+            //}
+        //}
+    //}
 }
+
+
+// 1.class board definir le board
+// 2. class cell pour verifier mur --> avoir crée un mur
